4544ab226d2e9a63d402e63932baf9e10d73b883
williams4027 <williams.4027@gmail.com>
2018-10-08 03:16:25 -0400

    Updated README with project structure and experimented with enzyme/jest tests.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # UpdoxCodingChallengeReact
 The React portion of the Updox Coding Challenge to create a Provider Directory.
+This was based on the starter create-react-app project and modified based on the project statement.
+This was implemented using a Redux pattern for easier maintainability as the app would grow.
+
+## Project Directory Structure
+-**src/** The main index.js file lives here, creating an initial state/redux store for rendering the app root
+-**src/css/** Holds all style assets
+-**src/js/** Holds all javascript assets
+--**actions/** Contains the redux actions used for the ProviderForm and ProviderTable to
+--**components/** All source code for react components
+--**constants/** Definition file to hold string constants for the Action classes for easier editing
+--**model/** Contains the ProviderData object for data transfer inside the ProviderForm
+--**reducers/** Holds all redux reducers, who take the actions and payloads and generates new states if needed
+--**store/** Simple redux store configuration to bring the pieces together
+
+# Run
+Ensure that npm is installed on the computer and run `npm install && npm start` from the project root.
+This should install all required node modules, and start up the React web development server.
+
+# Testing
+Due to time constraints, I was not able to add any meaningful test suites for the project.
+I would have liked to go in and learned how to create enzyme and jest tests to test the redux and
+regular component operations at a unit level but ran out of time.
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './src/js/components/App';
+import { Provider } from "react-redux";
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
+import App from './js/components/App';
+import ProviderTable from './js/components/ProviderTable';
+import configureStore from "./js/store/index";
+
+import ProviderData from "./js/model/ProviderData";
+import { providersTableToggleRow, providersRemove } from "./js/actions/providers";
+
+configure({ adapter: new Adapter() });
+
+it('>>>Initial React Test', () => {
+  const initialState = {
+    providers: [],
+    providersTable: {
+      selectedRows : []
+    },
+    providersForm: {
+      currentProvider : new ProviderData()
+    },
+  };
+
+  const store = configureStore(initialState);
+
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+    <App />
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('>>>Provider Table Tests',()=>{
+  const initialState = {
+    providers: [
+      {"last_name": "Harris", "first_name": "Mike", "email_address": "mharris@updox.com", "specialty": "Pediatrics", "practice_name": "Harris Pediatrics"},
+      {"last_name": "Wijoyo", "first_name": "Bimo", "email_address": "bwijoyo@updox.com", "specialty": "Podiatry", "practice_name": "Wijoyo Podiatry"},
+      {"last_name": "Rose", "first_name": "Nate", "email_address": "nrose@updox.com", "specialty": "Surgery", "practice_name": "Rose Cutters"},
+      {"last_name": "Carlson", "first_name": "Mike", "email_address": "mcarlson@updox.com", "specialty": "Orthopedics", "practice_name": "Carlson Orthopedics"},
+      {"last_name": "Witting", "first_name": "Mike", "email_address": "mwitting@updox.com", "specialty": "Pediatrics", "practice_name": "Witting’s Well Kids Pediatrics"},
+      {"last_name": "Juday", "first_name": "Tobin", "email_address": "tjuday@updox.com", "specialty": "General Medicine", "practice_name": "Juday Family Practice"},
+    ],
+    providersTable: {
+      selectedRows : []
+    },
+    providersForm: {
+      currentProvider : new ProviderData()
+    },
+  };
+  const mockStore = configureStore;
+
+  let store,container
+  beforeEach(()=>{
+    store = mockStore(initialState);
+    container = shallow(
+      <Provider store={store}>
+        <ProviderTable />
+      </Provider>,
+      document.createElement('div')
+    );
+  })
+
+  it('+++ render the provider table component', () => {
+    expect(container.length).toEqual(1)
+  });
 });
